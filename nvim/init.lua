a1affa8384259d9d5eb11ef597927e6670d0c90e
opjt <jtpark1957@gmail.com>
2025-09-30 07:31:56 +0900

    chore: clangd added

--- nvim/init.lua
+++ nvim/init.lua
@@ -769,6 +769,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clangd' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
