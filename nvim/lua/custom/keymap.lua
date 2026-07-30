d1f5be3eb088841b4df5b1313a67c42777a0c169
opjt <jtpark1957@gmail.com>
2026-07-30 10:03:46 +0900

    nvim: add buffer delete keymap using mini.bufremove

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -14,3 +14,8 @@ end
 
 -- 단축키 등록 (<leader>dd 로 예시)
 vim.keymap.set('n', '<leader>td', toggle_diagnostics, { noremap = true, silent = true, desc = 'Toggle Diagnostics' })
+
+-- 버퍼 닫기 (창 레이아웃은 유지한 채 버퍼만 제거: mini.bufremove)
+vim.keymap.set('n', '<leader>Q', function()
+  require('mini.bufremove').delete(0, false)
+end, { noremap = true, silent = true, desc = 'Buffer Delete' })
