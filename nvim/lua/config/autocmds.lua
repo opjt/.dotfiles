d0d24357a1987e9911ba8d40a40b394f1a6da7f7
opjt <jtpark1957@gmail.com>
2026-08-04 07:00:29 +0900

    nvim: markdown/explorer tweaks (disable render-markdown, conceal, lint, spellcheck; show hidden files)

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,6 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable spellcheck (red underline) in text filetypes (markdown, gitcommit, etc.)
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,11 @@
+return {
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
+}
